e95362333bc7276d02ca2c579e9563179b8992a6
Henrysilva12 <165195636+Henrysilva12@users.noreply.github.com>
2025-02-18 22:25:00 -0300

    RoadRunner -
    Programação de base para o período autônomo o roadrunner  e a maior parte das funcionalidades do atuador.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nacional/Autonomous_Mode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nacional/Autonomous_Mode.java
@@ -8,6 +8,7 @@ import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 
@@ -24,8 +25,7 @@ import org.firstinspires.ftc.teamcode.MecanumDrive;
 @Autonomous(name = "Teste RR", group = "Autonomous")
 public class Autonomous_Mode extends LinearOpMode {
 
-
-    //inicialização
+    //Initilização e criação de movimentos dos componentes do atuador
     public class leftandright {
         private DcMotorEx left, right;
         public leftandright(HardwareMap hardwareMap){
@@ -97,7 +97,7 @@ public class Autonomous_Mode extends LinearOpMode {
 
     }
 
-    //TODO: VERIFICAR COMO FUNCIONAM OS SERVOS
+    //TODO: VERIFICAR COMO FUNCIONAM OS SERVOS PARA ACERTAR OS MOVIMENTOS
     public class servos {
         private Servo garra, servoX, servoY;
         public servos(HardwareMap hardwareMap) {
@@ -147,15 +147,13 @@ public class Autonomous_Mode extends LinearOpMode {
 
     }
 
-    //TODO: CONTINUAR
-    public class slide {
+    //TODO: VERIFICAR OS ENCODERS DO GOBILDA PARA AJUSTES
+    public class Slide {
         private DcMotorEx gobilda;
-
-        public slide(HardwareMap hardwareMap) {
+        public Slide(HardwareMap hardwareMap) {
             gobilda = hardwareMap.get(DcMotorEx.class, "gobilda");
             gobilda.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         }
-
         public class slideExtender implements Action {
             private boolean initialized = false;
 
@@ -167,6 +165,7 @@ public class Autonomous_Mode extends LinearOpMode {
                 }
                 double pos = gobilda.getCurrentPosition();
                 packet.put("slidePos", pos);
+                //TODO: VERIFICAR
                 if(pos < 300.0) {
                     return true;
                 }
@@ -181,23 +180,110 @@ public class Autonomous_Mode extends LinearOpMode {
         public Action slideExtender(){
             return new slideExtender();
         }
+        public class slideCloser implements Action {
+            private boolean initialized = false;
 
-        public class slideCloser implements Action {}
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                if (!initialized) {
+                    gobilda.setPower(-1.0);
+                    initialized = true;
+                }
+                double pos = gobilda.getCurrentPosition();
+                packet.put("slidePos", pos);
+                //TODO: VERIFICAR
+                if (pos > 300.0) {
+                    return true;
+                } else {
+                    gobilda.setPower(0);
+                    return false;
+                }
+            }
+        }
+        public Action slideCloser(){
+            return new slideCloser();
+        }
     }
-
-
-
-
-
-    //ações
-
-
-
 
     @Override
     public void runOpMode(){
+        //inicializa o chassi em uma posição especifica
+        //TODO: VERIFICAR ESSA TAL POSIÇÂO
+        //Make sure your MecanumDrive is instantiated at the correct pose. If you end up using lineToX(), lineToY(),
+        //strafeTo(), splineTo(), or any of their variants in your code, if the initial pose is wrong,
+        // all future movements will be thrown off.
+        Pose2d initialPose = new Pose2d(11.8, 61.7,Math.toRadians(90));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
 
+        //cria uma instância para as o slide
+        Slide slide = new Slide(hardwareMap);
+        //cria uma instância para os motores
+        leftandright motors = new leftandright(hardwareMap);
+        //cria uma instância para os servos;
+        servos servos = new servos(hardwareMap);
+
+        //acho que isso é só para utilizar camera ent se pá vai ter q tirar
+        int visionOutputPosition = 1;
+
+        //TODO: A PARTIR DAQUI QUE È POSSÍVEL COLOCAR NOSSAS TRAJETÓRIAS
+        //TODO: ESTUDAR OQUE CADA EXPRESSÂO SIGNIFICA EXEMPLO: LINETOYSPLINEHEADING
+        TrajectoryActionBuilder tab1 = drive.actionBuilder(initialPose)
+                .lineToYSplineHeading(33, Math.toRadians(0))
+                .waitSeconds(2)
+                .setTangent(Math.toRadians(90))
+                .lineToY(48)
+                .setTangent(Math.toRadians(0))
+                .lineToX(32)
+                .strafeTo(new Vector2d(44.5, 30))
+                .turn(Math.toRadians(180))
+                .lineToX(47.5)
+                .waitSeconds(3);
+        TrajectoryActionBuilder tab2 = drive.actionBuilder(initialPose)
+                .lineToY(37)
+                .setTangent(Math.toRadians(0))
+                .lineToX(18)
+                .waitSeconds(3)
+                .setTangent(Math.toRadians(0))
+                .lineToXSplineHeading(46, Math.toRadians(180))
+                .waitSeconds(3);
+        TrajectoryActionBuilder tab3 = drive.actionBuilder(initialPose)
+                .lineToYSplineHeading(33, Math.toRadians(180))
+                .waitSeconds(2)
+                .strafeTo(new Vector2d(46, 30))
+                .waitSeconds(3);
+        Action trajectoryActionCloseOut = tab1.endTrajectory().fresh()
+                .strafeTo(new Vector2d(48, 12))
+                .build();
+
+        //ações que acontecem ao inicializar
+        Actions.runBlocking(servos.GarraOpen());
+
+        while(!isStopRequested() && !opModeIsActive()){
+            int position = visionOutputPosition;
+            telemetry.addData("Position durint INIT", position);
+            telemetry.update();
+        }
+        int startPosition = visionOutputPosition;
+        telemetry.addData("Startinng Position", startPosition);
+        telemetry.update();
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        Action trajectoryActionChosen;
+        if (startPosition == 1) {
+            trajectoryActionChosen = tab1.build();
+        } else if (startPosition == 2) {
+            trajectoryActionChosen = tab2.build();
+        }else {
+            trajectoryActionChosen = tab3.build();
+        }
+        Actions.runBlocking(
+                new SequentialAction(
+                        trajectoryActionChosen,
+                        servos.GarraOpen(),
+                        trajectoryActionCloseOut
+                )
+        );
     }
-
-
 }
